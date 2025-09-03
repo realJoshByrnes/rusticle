512c358bdee85b61e077b63a9b71ebbe5bd2d8bc
realJoshByrnes <204185+realJoshByrnes@users.noreply.github.com>
2025-09-03 12:41:00 +1000

    Replaced Rust's default `main()` with a custom `mainCRTStartup` entry point.
    - Added `#![no_main]` and defined `mainCRTStartup` with `extern "C"` and `-> !`
    - Added linker flags: `/ENTRY:mainCRTStartup` and `/SUBSYSTEM:CONSOLE`
    - Manually linked `vcruntime` to resolve missing CRT symbols

---
+++ build.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let profile = std::env::var("PROFILE").unwrap_or_default();
+
+    if profile == "release" {
+        println!("cargo:rustc-link-arg=/ENTRY:mainCRTStartup");
+        println!("cargo:rustc-link-arg=/SUBSYSTEM:CONSOLE"); // or WINDOWS
+        println!("cargo:rustc-link-lib=vcruntime");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
+#![no_main]
+
+use std::process::exit;
+
+#[unsafe(no_mangle)]
+pub extern "C" fn mainCRTStartup() -> ! {
     println!("Hello, world!");
+    exit(0)
 }
