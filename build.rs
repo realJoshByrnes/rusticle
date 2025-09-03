8d13f96be2deff70b178cd893d1483bdbba2b764
realJoshByrnes <204185+realJoshByrnes@users.noreply.github.com>
2025-09-03 23:54:53 +1000

    Fix dev build flags

--- build.rs
+++ build.rs
@@ -1,10 +1,11 @@
 fn main() {
     let profile = std::env::var("PROFILE").unwrap_or_default();
 
+    println!("cargo:rustc-link-arg=/ENTRY:mainCRTStartup");
+    println!("cargo:rustc-link-arg=/SUBSYSTEM:CONSOLE");
+    println!("cargo:rustc-link-lib=kernel32");
+
     if profile == "release" {
-        println!("cargo:rustc-link-arg=/ENTRY:mainCRTStartup");
-        println!("cargo:rustc-link-arg=/SUBSYSTEM:CONSOLE");
-        println!("cargo:rustc-link-lib=kernel32");
         println!("cargo:rustc-link-arg=/ALIGN:8");
         println!("cargo:rustc-link-arg=/DEBUG:NONE");
         println!("cargo:rustc-link-arg=/SAFESEH:NO");
