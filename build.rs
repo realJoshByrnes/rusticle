554f6ca94beb30e43e40d70e1806f00263f6cd31
Josh Byrnes <204185+realJoshByrnes@users.noreply.github.com>
2025-09-14 09:16:11 +1000

    Bump version to 1.20.1; update build script and main.rs for clarity and organization
    - Removed linker flags that were not required to reduce binary size
    - Added better comments

--- build.rs
+++ build.rs
@@ -2,24 +2,22 @@ fn main() {
     let profile = std::env::var("PROFILE").unwrap_or_default();
     let bitness = std::env::var("CARGO_CFG_TARGET_POINTER_WIDTH").unwrap_or_default();
 
+    // We don't use the Microsoft Visual C++ Redistributable, so we set our custom entry point
     println!("cargo:rustc-link-arg=/ENTRY:mainCRTStartup");
+    // We set our compatibility with Windows XP+
     println!("cargo:rustc-link-arg=/SUBSYSTEM:CONSOLE,5.01");
-    println!("cargo:rustc-link-lib=kernel32");
 
     if profile == "release" {
         if bitness == "32" {
-            println!("cargo:rustc-link-arg=/ALIGN:4");
+            println!("cargo:rustc-link-arg=/ALIGN:4"); // i686
         } else {
-            println!("cargo:rustc-link-arg=/ALIGN:8");
+            println!("cargo:rustc-link-arg=/ALIGN:8"); // x86_64
         }
-        println!("cargo:rustc-link-arg=/DEBUG:NONE");
-        println!("cargo:rustc-link-arg=/SAFESEH:NO");
-        println!("cargo:rustc-link-arg=/MERGE:.pdata=.text");
-        println!("cargo:rustc-link-arg=/STUB:dos_stub.bin");
-        println!("cargo:rustc-link-arg=/EMITTOOLVERSIONINFO:NO");
-        println!("cargo:rustc-link-arg=/RELEASE");
-        println!("cargo:rustc-link-arg=/EMITPOGOPHASEINFO");
-        println!("cargo:rustc-link-arg=/FIXED");
-        println!("cargo:rustc-link-arg=/DYNAMICBASE:NO");
+        println!("cargo:rustc-link-arg=/DEBUG:NONE"); // Both
+        println!("cargo:rustc-link-arg=/MERGE:.pdata=.text"); // x86_64
+        println!("cargo:rustc-link-arg=/STUB:dos_stub.bin"); // Both
+        println!("cargo:rustc-link-arg=/EMITTOOLVERSIONINFO:NO"); // Both
+        println!("cargo:rustc-link-arg=/EMITPOGOPHASEINFO"); // Both
+        println!("cargo:rustc-link-arg=/FIXED"); // i686
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 
 use core::{ffi::c_void, panic::PanicInfo, ptr::null_mut};
 
+// Import Win32 API items (from kernel32.lib / kernel32.dll)
 #[link(name = "kernel32")]
 unsafe extern "system" {
     fn WriteFile(
@@ -14,27 +15,32 @@ unsafe extern "system" {
     ) -> i32;
 }
 
+// This is a special constant which can be passed to some Win32 API's to get the standard output handle
 const STD_OUTPUT_HANDLE: u32 = -11i32 as u32;
 
+// By moving the HELLO_WORLD_MESSAGE to .text, we save 4 bytes on i686
 #[unsafe(link_section = ".text")]
-pub static HELLO_WORLD_MESSAGE: [u8; 14] = *b"Hello, world!\n";
+// This is the actual message we'll print to the console
+static HELLO_WORLD_MESSAGE: [u8; 14] = *b"Hello, world!\n";
 
 #[unsafe(no_mangle)]
+// mainCRTStartup should return `!`, but Windows allows us to return a `u32` to exit the current thread and provide an exit code
 pub extern "C" fn mainCRTStartup() -> u32 {
     unsafe {
-        let handle = STD_OUTPUT_HANDLE as *mut c_void;
+        // Write our HELLO_WORLD_MESSAGE to the console
         WriteFile(
-            handle,
+            STD_OUTPUT_HANDLE as _,
             HELLO_WORLD_MESSAGE.as_ptr(),
             HELLO_WORLD_MESSAGE.len() as u32,
             null_mut(),
             null_mut(),
         );
-        // Returning 0 from mainCRTStartup is equivalent to calling ExitProcess(0) if only one thread is running.
+        // Returning 0 (SUCCESS) is equivalent to calling ExitProcess(0) if only one thread is running.
         0
     }
 }
 
+// Even though we don't use a panic handler, rust still requires one (dummy implementation)
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
